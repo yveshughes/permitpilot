06f7c836aab8a0f26331ee7deca4b797e601a7d5
Yves Hughes <yves@nextdoormultimedia.com>
2024-11-09 14:58:55 -0800

    fixing deployment issue

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+// src/components/stat.tsx
+import { Badge } from '@/components/badge'
+import { Divider } from '@/components/divider'
 
-interface StatProps {
-  title: string
-  value: string | number
-  change?: string | number
-}
-
-export function Stat({ title, value, change }: StatProps) {
+export function Stat({ title, value, change }: { title: string; value: string; change: string }) {
   return (
-    <div className="stat">
-      <h3>{title}</h3>
-      <p>{value}</p>
-      {change && <span>{change}</span>}
+    <div>
+      <Divider />
+      <div className="mt-6 text-lg/6 font-medium sm:text-sm/6">{title}</div>
+      <div className="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{value}</div>
+      <div className="mt-3 text-sm/6 sm:text-xs/6">
+        <Badge color={change.startsWith('+') ? 'lime' : 'pink'}>{change}</Badge>{' '}
+        <span className="text-zinc-500">from last week</span>
+      </div>
     </div>
   )
 }
